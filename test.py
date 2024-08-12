32eb8ceeeed19664450c6fb96a4a3f468719deb3
Kulmyaev <inkulmyaev@gmail.com>
2024-08-12 16:29:44 +0300

    test 3

---
+++ test.py
@@ -0,0 +1,5 @@
+num1, num2 = input("num1 "), input("num2 ")
+print(f"num1 = {num1}, num2 = {num2}")
+"""Поменяем значения num1 и num2"""
+num1, num2 = num2, num1
+print(f"num1 = {num1}, num2 = {num2}")
