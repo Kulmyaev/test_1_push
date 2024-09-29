9a1fb3132a8af2cdcd22c04db86feb438130a448
Kulmyaev <inkulmyaev@gmail.com>
2024-09-30 00:08:38 +0300

    Created a file Program. Added two functions and Variable Annotation - 1

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # функция
 def hello():
     print("Hello world")
@@ -13,3 +15,7 @@ sity: str = 'Gamora'
 
 focus(name,sity)
 
+name: str = 'Tommy'
+age: int = 24
+height_in_meters: float = 1.7
+colleagues: List[str] = ['Alicia', 'John']
