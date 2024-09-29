e58859af47fb9ad63553abfe6447ab66e1533886
Kulmyaev <inkulmyaev@gmail.com>
2024-09-30 00:06:47 +0300

    Created a file Program. Added two functions and Variable Annotation

---
+++ program.py
@@ -0,0 +1,15 @@
+# функция
+def hello():
+    print("Hello world")
+
+hello()
+
+# Аннотация переменных
+def focus(name: str,sity: str):
+    print(f" {name} or {sity} ")
+
+name: str = 'Jak'
+sity: str = 'Gamora'
+
+focus(name,sity)
+
